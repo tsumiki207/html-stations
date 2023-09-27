b02942d3d964694c6b01f4c405b007092069a4e6
tsumiki207 <s2122047@stu.musashino-u.ac.jp>
2023-09-28 00:59:04 +0900

    hi

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,16 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const result = await userList.map(test);
   return await result
+
+
+  function test(data) {
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    data.full_name =  data.family_name + ' ' + data.first_name;
+    return data;
+  }
 }
 
-function test() {
-  return
-}
 
